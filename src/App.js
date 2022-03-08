bb501ee1b90339701892079e2bb37338c51892fa
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-08 15:32:24 -0800

    gangshoot the image filepaths

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Footer from './components/Footer/Footer.js';
+import Main from './components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <Footer />
     </div>
   );
