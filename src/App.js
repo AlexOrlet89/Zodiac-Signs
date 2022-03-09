8de9ebbdcf4b3e2472bf315f02995416e7a84182
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-08 16:19:30 -0800

    CSS added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer.js';
 import Main from './components/Main/Main.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
       <Footer />
     </div>
